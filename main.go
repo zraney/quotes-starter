98c14ed5ee3c026843e6bce41e99447ce266500c
Zack Raney <raney.zd@c02w48m4htd6.localdomain>
2022-09-21 11:26:44 -0400

    changed key type to string uuid instead of int. re-worked randomquote func.

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 )
 
 type quote struct {
-	Id     string `json:"id"`
+	ID     string `json:"id"`
 	Quote  string `json:"quote"`
 	Author string `json:"author"`
 }
@@ -21,19 +21,24 @@ func main() {
 }
 
 func getRandomQuote(c *gin.Context) {
-	randomKey := rand.Intn(len(quotes))
-	pick := quotes[randomKey]
-	c.JSON(http.StatusOK, pick)
+	keyArray := []string{}
+	for k, _ := range quotes {
+		keyArray = append(keyArray, k)
+	}
+	randomIndex := rand.Intn(len(keyArray))
+	randomPick := keyArray[randomIndex]
+	randomQuote := quotes[randomPick]
+	c.JSON(http.StatusOK, randomQuote)
 }
 
-var quotes = map[int]quote{
-	1: {Id: "b37c9ded-d176-4fe5-a9b9-1427ebf92ed1", Quote: "Errors are values.", Author: "Rob Pike"},
-	2: {Id: "0d95d2d8-28b0-4278-960d-cbdd16beab02", Quote: "Errors are values.", Author: "Rob Pike"},
-	3: {Id: "0329b963-004d-4add-bb5e-cfe7defd0c6d", Quote: "Don't panic.", Author: "Go Code Review Comments"},
-	4: {Id: "2e774b8c-672e-46bf-8b6f-38d6889edee7", Quote: "A little copying is better than a little dependency.", Author: "Rob Pike"},
-	5: {Id: "a2ad7811-22ea-4ba4-8691-a88b5f89a475", Quote: "Concurrency is not parallelism.", Author: "Rob Pike"},
-	6: {Id: "ba9b4b54-3070-4665-bd29-de3e99c991d2", Quote: "interface{} says nothing.", Author: "Rob Pike"},
-	7: {Id: "ca17bd05-4c0b-41ae-9496-518371e245f2", Quote: "Make the zero value useful.", Author: "Rob Pike"},
+var quotes = map[string]quote{
+	"b37c9ded-d176-4fe5-a9b9-1427ebf92ed1": {ID: "b37c9ded-d176-4fe5-a9b9-1427ebf92ed1", Quote: "Errors are values.", Author: "Rob Pike"},
+	"0d95d2d8-28b0-4278-960d-cbdd16beab02": {ID: "0d95d2d8-28b0-4278-960d-cbdd16beab02", Quote: "Errors are values.", Author: "Rob Pike"},
+	"0329b963-004d-4add-bb5e-cfe7defd0c6d": {ID: "0329b963-004d-4add-bb5e-cfe7defd0c6d", Quote: "Don't panic.", Author: "Go Code Review Comments"},
+	"2e774b8c-672e-46bf-8b6f-38d6889edee7": {ID: "2e774b8c-672e-46bf-8b6f-38d6889edee7", Quote: "A little copying is better than a little dependency.", Author: "Rob Pike"},
+	"a2ad7811-22ea-4ba4-8691-a88b5f89a475": {ID: "a2ad7811-22ea-4ba4-8691-a88b5f89a475", Quote: "Concurrency is not parallelism.", Author: "Rob Pike"},
+	"ba9b4b54-3070-4665-bd29-de3e99c991d2": {ID: "ba9b4b54-3070-4665-bd29-de3e99c991d2", Quote: "interface{} says nothing.", Author: "Rob Pike"},
+	"ca17bd05-4c0b-41ae-9496-518371e245f2": {ID: "ca17bd05-4c0b-41ae-9496-518371e245f2", Quote: "Make the zero value useful.", Author: "Rob Pike"},
 }
 
 func Add() {
