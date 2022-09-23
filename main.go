5c2c0210e7a68eff960eb2e6b32ec9de0d3e0c54
Zack Raney <raney.zd@c02w48m4htd6.localdomain>
2022-09-23 11:13:13 -0400

    updated status codes and addNewQuote now returns correctly

--- main.go
+++ main.go
@@ -9,10 +9,10 @@ import (
 	"github.com/google/uuid"
 )
 
-type quote struct {
+type Quote struct {
 	ID     string `json:"id"`
-	Quote  string `json:"quote"`
-	Author string `json:"author"`
+	Quote  string `json:"quote,omitempty"`
+	Author string `json:"author,omitempty"`
 }
 
 func main() {
@@ -56,7 +56,7 @@ func getQuoteByID(c *gin.Context) {
 			return
 		}
 
-		c.JSON(http.StatusNotFound, gin.H{"message": "quote not found"})
+		c.JSON(http.StatusBadRequest, gin.H{"message": "invalid id"})
 		return
 	}
 	c.JSON(http.StatusUnauthorized, gin.H{"message": "not authorized"})
@@ -66,7 +66,7 @@ func getQuoteByID(c *gin.Context) {
 func addNewQuote(c *gin.Context) {
 	if handleRequest(c) {
 		newID := uuid.New().String()
-		var newQuote quote
+		var newQuote Quote
 		newQuote.ID = newID
 
 		if err := c.BindJSON(&newQuote); err != nil {
@@ -74,19 +74,22 @@ func addNewQuote(c *gin.Context) {
 		}
 		if len(newQuote.Author) >= 3 && len(newQuote.Quote) >= 3 {
 			quotes[newQuote.ID] = newQuote
-			c.JSON(http.StatusCreated, newQuote.ID)
+			returnId := Quote{
+				ID: newQuote.ID,
+			}
+			c.JSON(http.StatusCreated, returnId)
 			return
 		}
-		c.JSON(http.StatusNotFound, gin.H{"message": "quote and author must be greater than 3 characters"})
+		c.JSON(http.StatusBadRequest, gin.H{"message": "quote and author must be greater than 3 characters"})
 		return
 	}
 	c.JSON(http.StatusUnauthorized, gin.H{"message": "not authorized"})
 
 }
 
-var quotes = map[string]quote{
+var quotes = map[string]Quote{
 	"b37c9ded-d176-4fe5-a9b9-1427ebf92ed1": {ID: "b37c9ded-d176-4fe5-a9b9-1427ebf92ed1", Quote: "Errors are values.", Author: "Rob Pike"},
-	"0d95d2d8-28b0-4278-960d-cbdd16beab02": {ID: "0d95d2d8-28b0-4278-960d-cbdd16beab02", Quote: "Errors are values.", Author: "Rob Pike"},
+	"0d95d2d8-28b0-4278-960d-cbdd16beab02": {ID: "0d95d2d8-28b0-4278-960d-cbdd16beab02", Quote: "Clear is better than clever.", Author: "Rob Pike"},
 	"0329b963-004d-4add-bb5e-cfe7defd0c6d": {ID: "0329b963-004d-4add-bb5e-cfe7defd0c6d", Quote: "Don't panic.", Author: "Go Code Review Comments"},
 	"2e774b8c-672e-46bf-8b6f-38d6889edee7": {ID: "2e774b8c-672e-46bf-8b6f-38d6889edee7", Quote: "A little copying is better than a little dependency.", Author: "Rob Pike"},
 	"a2ad7811-22ea-4ba4-8691-a88b5f89a475": {ID: "a2ad7811-22ea-4ba4-8691-a88b5f89a475", Quote: "Concurrency is not parallelism.", Author: "Rob Pike"},
