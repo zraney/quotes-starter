a1d47d21b15f0bfce56b24f60fc38a6a989b8b32
Zack Raney <raney.zd@c02w48m4htd6.localdomain>
2022-09-30 14:26:48 -0400

    refactored post func. added delete func. and cleaned up code.

--- main.go
+++ main.go
@@ -22,16 +22,15 @@ type Quote struct {
 var db *sql.DB
 
 func main() {
-
 	err := connectUnixSocket()
 	if err != nil {
 		log.Fatalln(err)
 	}
-
 	router := gin.Default()
 	router.GET("/quotes", getRandomQuote)
 	router.GET("/quotes/:id", getQuoteByID)
 	router.POST("/quotes", addNewQuote)
+	router.DELETE("/quotes/:id", deleteQuoteByID)
 	router.Run("0.0.0.0:8080")
 }
 
@@ -73,7 +72,7 @@ func handleRequest(c *gin.Context) bool {
 
 func getRandomQuote(c *gin.Context) {
 	if handleRequest(c) {
-		row := db.QueryRow("SELECT id, phrase, author FROM quotes ORDER BY RANDOM() LIMIT 1")
+		row := db.QueryRow("SELECT id, phrase, author FROM quotes ORDER BY RANDOM() LIMIT 1;")
 		quote := &Quote{}
 		err := row.Scan(&quote.ID, &quote.Quote, &quote.Author)
 		if err != nil {
@@ -85,28 +84,10 @@ func getRandomQuote(c *gin.Context) {
 	c.JSON(http.StatusUnauthorized, gin.H{"message": "not authorized"})
 }
 
-// TODO break this func into smaller, more focused funcs
-// func getRandomQuote(c *gin.Context) {
-// 	if handleRequest(c) {
-// 		keyArray := []string{}
-// 		for k, _ := range quotes {
-// 			keyArray = append(keyArray, k)
-// 		}
-// 		randomIndex := rand.Intn(len(keyArray))
-// 		randomPick := keyArray[randomIndex]
-// 		randomQuote := quotes[randomPick]
-// 		c.JSON(http.StatusOK, randomQuote)
-// 		return
-// 	}
-// 	c.JSON(http.StatusUnauthorized, gin.H{"message": "not authorized"})
-// }
-
-// TODO auth and invalid id not working
-// .query may return 2 variables
 func getQuoteByID(c *gin.Context) {
 	if handleRequest(c) {
 		id := c.Param("id")
-		row := db.QueryRow(fmt.Sprintf("SELECT id, phrase, author FROM quotes WHERE id = '%s'", id))
+		row := db.QueryRow(fmt.Sprintf("SELECT id, phrase, author FROM quotes WHERE id = '%s';", id))
 		quote := &Quote{}
 		err := row.Scan(&quote.ID, &quote.Quote, &quote.Author)
 		if err != nil {
@@ -122,23 +103,6 @@ func getQuoteByID(c *gin.Context) {
 	c.JSON(http.StatusUnauthorized, gin.H{"message": "not authorized"})
 }
 
-// func getQuoteByID(c *gin.Context) {
-// 	if handleRequest(c) {
-// 		id := c.Param("id")
-// 		quote, exists := quotes[id]
-
-// 		if exists {
-// 			c.JSON(http.StatusOK, quote)
-// 			return
-// 		}
-
-// 		c.JSON(http.StatusBadRequest, gin.H{"message": "invalid id"})
-// 		return
-// 	}
-// 	c.JSON(http.StatusUnauthorized, gin.H{"message": "not authorized"})
-
-// }
-
 func addNewQuote(c *gin.Context) {
 	if handleRequest(c) {
 		newID := uuid.New().String()
@@ -148,10 +112,19 @@ func addNewQuote(c *gin.Context) {
 		if err := c.BindJSON(&newQuote); err != nil {
 			return
 		}
-		if len(newQuote.Author) >= 3 && len(newQuote.Quote) >= 3 {
-			quotes[newQuote.ID] = newQuote
-			returnId := Quote{
-				ID: newQuote.ID,
+		id := newQuote.ID
+		phrase := newQuote.Quote
+		author := newQuote.Author
+		returnId := Quote{
+			ID: newQuote.ID,
+		}
+
+		if len(author) >= 3 && len(phrase) >= 3 {
+			_, err := db.Exec(fmt.Sprintf("INSERT INTO quotes (id, phrase, author) VALUES ('%s', '%s', '%s');", id, phrase, author))
+
+			if err != nil {
+				c.JSON(http.StatusBadRequest, gin.H{"message": err})
+				return
 			}
 			c.JSON(http.StatusCreated, returnId)
 			return
@@ -160,15 +133,19 @@ func addNewQuote(c *gin.Context) {
 		return
 	}
 	c.JSON(http.StatusUnauthorized, gin.H{"message": "not authorized"})
-
 }
 
-var quotes = map[string]Quote{
-	"b37c9ded-d176-4fe5-a9b9-1427ebf92ed1": {ID: "b37c9ded-d176-4fe5-a9b9-1427ebf92ed1", Quote: "Errors are values.", Author: "Rob Pike"},
-	"0d95d2d8-28b0-4278-960d-cbdd16beab02": {ID: "0d95d2d8-28b0-4278-960d-cbdd16beab02", Quote: "Clear is better than clever.", Author: "Rob Pike"},
-	"0329b963-004d-4add-bb5e-cfe7defd0c6d": {ID: "0329b963-004d-4add-bb5e-cfe7defd0c6d", Quote: "Don't panic.", Author: "Go Code Review Comments"},
-	// "2e774b8c-672e-46bf-8b6f-38d6889edee7": {ID: "2e774b8c-672e-46bf-8b6f-38d6889edee7", Quote: "A little copying is better than a little dependency.", Author: "Rob Pike"},
-	// "a2ad7811-22ea-4ba4-8691-a88b5f89a475": {ID: "a2ad7811-22ea-4ba4-8691-a88b5f89a475", Quote: "Concurrency is not parallelism.", Author: "Rob Pike"},
-	// "ba9b4b54-3070-4665-bd29-de3e99c991d2": {ID: "ba9b4b54-3070-4665-bd29-de3e99c991d2", Quote: "interface{} says nothing.", Author: "Rob Pike"},
-	// "ca17bd05-4c0b-41ae-9496-518371e245f2": {ID: "ca17bd05-4c0b-41ae-9496-518371e245f2", Quote: "Make the zero value useful.", Author: "Rob Pike"},
+func deleteQuoteByID(c *gin.Context) {
+	if handleRequest(c) {
+		id := c.Param("id")
+		_, err := db.Exec(fmt.Sprintf("DELETE FROM quotes WHERE id = '%s';", id))
+
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"message": err})
+			return
+		}
+		c.JSON(http.StatusNoContent, gin.H{"message": "204 can't display a message"})
+		return
+	}
+	c.JSON(http.StatusUnauthorized, gin.H{"message": "not authorized"})
 }
